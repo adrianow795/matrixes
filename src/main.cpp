632c1924f696ff39a1dd3c0b6c0389e2c4d8fb23
adrianow795 <adrianow795@gmail.com>
2025-04-05 08:55:17 +0200

    fibo

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,44 @@
 #include <cstdlib>
 #include <ctime>
 
+
+constexpr int fibo(int el)
+{
+    long long int result = 0;
+    if(el == 1)
+    {
+        result = 1;
+    }
+    else if(el > 1)
+    {
+        int a = 0, b = 1;
+        result = a +b;
+        for(int i = 2; i < el; i++)
+        {
+            a = b;
+            b = result;
+            result = a + b;
+        }
+    }
+    return result;
+}
+
+constexpr int fiboRec(int el)
+{
+    if(el == 0)
+    {
+        return 0;
+    }
+    if(el == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fiboRec(el-1) + fiboRec(el-2);
+    }
+}
+
 int main()
 {
     constexpr auto x = 3;
@@ -25,6 +63,28 @@ int main()
     m2.show("m2");
     m3 = m1 + m2; //RVO return value opt
     m3.show("m3");
+
+    #define ELEMENT_NO 20
+    time_t start_time, end_time;
+    double duration;
+    time(&start_time);
+    constexpr long long int el = fibo(ELEMENT_NO);
+    time(&end_time);
+    duration = difftime(end_time, start_time);
+    std::cout<< "\nfibo[" << ELEMENT_NO << "] = " << el;
+
+    time(&start_time);
+    constexpr long long int el2 = fiboRec(ELEMENT_NO);
+    time(&end_time);
+    duration = difftime(end_time, start_time);
+    std::cout<< "\nfibo[" << ELEMENT_NO << "] = " << el2;
+
+    // int y = 0;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     std::cout<< "\nfibo[" << y << "] = " << fibo(i) << " - " << fiboRec(i);
+    // }
+   
 
     return 0;
 }
